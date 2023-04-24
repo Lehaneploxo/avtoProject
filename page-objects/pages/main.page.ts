e550f183b33ac15cb0ba344f329b20dfe4ab6ccf
Lehaneploxo <lehatvel@gmail.com>
2023-04-24 16:25:26 +0300

    24.04.2023commit

--- page-objects/pages/main.page.ts
+++ page-objects/pages/main.page.ts
@@ -5,6 +5,13 @@ interface IMainPageClick {
   aboutSushi?: null
   oplata?: null
   instruction?: null
+  pdfMenu?: null
+  requestCall?: null
+  enter?: null
+  basket?: null 
+  menu?: null
+  novelties?: null
+  popular?: null
 }
 
 interface IMainPage {
@@ -15,12 +22,27 @@ class MainPage extends BasePage {
   private aboutSushi: Button
   private oplata: Button
   private instruction: Button
+  private pdfMenu: Button
+  private requestCall: Button
+  private enter: Button
+  private basket: Button
+  private menu: Button
+  private novelties: Button
+  private popular: Button
 
   constructor() {
     super('body', 'Bento main page')
     this.aboutSushi = new Button('.top_nav [href="/page/about/"]', 'Sushi button')
-    this.oplata = new Button('.top_nav [href="/page/payment-and-delivery/""]', 'Delivery button')
+    this.oplata = new Button('.top_nav [href="/page/payment-and-delivery/"]', 'Delivery button')
     this.instruction = new Button('.top_nav [href="/page/instrukciya-po-registracii-p8/"]', 'Instruction button')
+    this.pdfMenu = new Button('.site_button', 'PDF Menu button')
+    this.requestCall = new Button('.contactCenterBtn', 'Request Call button')
+    this.enter = new Button('.authorizationLink', 'Enter button')
+    this.basket = new Button('.cartBlock', 'Basket button')
+    this.menu = new Button('.catalogBlock', 'Menu button')
+    this.novelties = new Button('#tab-1', 'Novelties button')
+    this.popular = new Button('#tab-2', 'Popular button')
+    
   }
 }
 
--- specs/main.page.testing.spec.ts
+++ specs/main.page.testing.spec.ts
@@ -7,5 +7,13 @@ describe('Main page testing', () => {
     await browser.url('/')
     await mainPage.click({aboutSushi: null, instruction: null})
   });
+  it('Should go to main page', async () => {
+    await browser.url('/')
+    await mainPage.click({oplata: null, instruction: null})
+  });
+  it.only('Should go to main page', async () => {
+    await browser.url('/')
+    await mainPage.click({popular: null})
+  });
 });
 
