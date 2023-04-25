254f920514443fa953915bf8bc851fe613451fbe
Lehaneploxo <110493196+Lehaneploxo@users.noreply.github.com>
2023-04-25 17:49:51 +0300

    April24 (#5)
    * 24.04.2023commit
    
    * исправил ошибки
    
    * исправил

--- specs/main.page.testing.spec.ts
+++ specs/main.page.testing.spec.ts
@@ -7,5 +7,15 @@ describe('Main page testing', () => {
     await browser.url('/')
     await mainPage.click({aboutSushi: null, instruction: null})
   });
+
+  it('Should go to main page', async () => {
+    await browser.url('/')
+    await mainPage.click({oplata: null, instruction: null})
+  });
+
+  it.only('Should go to main page', async () => {
+    await browser.url('/')
+    await mainPage.click({heart: null})
+  });
 });
 
